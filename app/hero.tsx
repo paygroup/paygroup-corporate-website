bd3c189f930b7e40b14a8f8fd413f45d69008335
sohanemon <m.sohanemon@gmail.com>
2023-07-21 17:01:20 +0000

    Jul 21, 2023, 11:01 PM

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,9 +1,15 @@
+import { Typography } from '@/components/ui/typography';
 import Image from 'next/image';
 
 export default function Hero() {
   return (
     <section className='grid items-center grid-cols-1 md:grid-cols-2 justify-items-center'>
-      <div></div>
+      <div>
+        <Typography>
+          PayGroup is a digital platform that enables social groups living in d
+          ifferent countries, continents to:{' '}
+        </Typography>
+      </div>
       <Image src={`/assets/images/phone.png`} alt='' width={400} height={500} />
     </section>
   );
