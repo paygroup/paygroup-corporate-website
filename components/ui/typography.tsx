f1daf031d4e95f7387c707ddd676e8d3a256120f
sohanemon <m.sohanemon@gmail.com>
2023-07-21 17:00:19 +0000

    Jul 21, 2023, 11:00 PM

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,11 +1,13 @@
 import { cn } from '@/lib/utils';
 import { HtmlHTMLAttributes, forwardRef } from 'react';
 
-interface TypographyProps extends HtmlHTMLAttributes<HTMLHeadingElement> {}
+interface TypographyProps extends HtmlHTMLAttributes<HTMLHeadingElement> {
+  heading?: boolean;
+}
 
 const Typography = forwardRef<HTMLHeadingElement, TypographyProps>(
   ({ className, ...props }, ref) => {
-    return <h1 ref={ref} {...props} className={cn('', {}, className)} />;
+    return <h1 ref={ref} {...props} className={cn('text-xl', {}, className)} />;
   }
 );
 
