7b8f94bc5a0a546be75aad9814ebd67334e48b15
sohanemon <m.sohanemon@gmail.com>
2023-07-21 16:44:43 +0000

    Jul 21, 2023, 10:44 PM

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+
 export default function Hero() {
-  return <section className='grid grid-cols-1 md:grid-cols-2'></section>;
+  return (
+    <section className='grid items-center grid-cols-1 md:grid-cols-2 justify-items-center'>
+      <div></div>
+      <Image src={`/assets/images/phone.png`} alt='' width={400} height={500} />
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from './hero';
+
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className=''>
+      <Hero />
+    </main>
+  );
 }
