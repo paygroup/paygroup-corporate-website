648e00390b570d04a68bc9a48862609b8de03c62
sohanemon <m.sohanemon@gmail.com>
2023-07-22 04:23:38 +0000

    Jul 22, 2023, 10:23 AM

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,17 +1,32 @@
 import Brand from '@/components/ui/brand';
 import Image from 'next/image';
 import Link from 'next/link';
+import Social from './social';
 
 export default function Footer() {
   return (
-    <section className='container flex flex-wrap justify-between gap-10 px-3 py-10 mt-32'>
-      <div className='space-y-4'>
-        <Brand />
-        <p className='text-neutral-800 text-[14px] font-normal leading-snug'>
-          A digital platform that enables social groups.
-        </p>
+    <section className='bg-secondary/30'>
+      <div className='container flex flex-wrap justify-between gap-10 px-3 py-10 mt-32 text-neutral-800'>
+        <div className='space-y-4'>
+          <Brand />
+          <p className=' text-[14px] font-normal leading-snug'>
+            A digital platform that enables social groups.
+          </p>
+          <Social />
+        </div>
+        {/* middle part */}
+        <div className='grid grid-cols-1 capitalize md:grid-cols-2 '>
+          {data.map((_) => (
+            <Link key={_.label} href={_.href} className='hover:text-primary'>
+              {_.label}
+            </Link>
+          ))}
+        </div>
       </div>
-      {/* middle part */}
+
+      <p className='pt-4 pb-10 text-center'>
+        © 2023 T-Cube Africa sarl. All rights reserved 
+      </p>
     </section>
   );
 }
@@ -22,20 +37,21 @@ const data = [
     href: '/',
   },
   {
-    label: 'about us',
-    href: '/about',
-  },
-  {
-    label: 'contact us',
-    href: '/contact',
-  },
-  {
     label: 'terms & condition of service',
     href: '/',
   },
   {
+    label: 'about us',
+    href: '/about',
+  },
+
+  {
     label: 'Confidentiality Policy ',
     href: '/',
+  },
+  {
+    label: 'contact us',
+    href: '/contact',
   },
   {
     label: 'FAQ',
